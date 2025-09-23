a4124010191b81d05e0a347bb2bd0a120d9016b4
Daniel <danieldelarosaquintana3@gmail.com>
2025-09-23 18:08:49 +0200

    APP GESTION added

---
+++ GestionClientes/src/main/java/com/mycompany/gestionclientes/Cliente.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gestionclientes;
+
+import java.util.Date;
+
+/**
+ *
+ * @author danie
+ */
+public class Cliente {
+    private String  nombre ,apellidos ,provincia;
+    private Date fecha;
+
+    public Cliente(String nombre, String apellidos, String provincia, Date fecha) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.provincia = provincia;
+        this.fecha = fecha;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nombre=" + nombre + ", apellidos=" + apellidos + ", provincia=" + provincia + ", fecha=" + fecha + '}';
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+    
+    public String[] toArrayString() {
+        String[] retorna =  new String[4];
+        retorna[0] =  nombre;
+        retorna[1] =  apellidos;
+        retorna[2] =  fecha.toString();    
+        retorna[3] =  provincia;
+        return retorna;
+    }
+}
---
+++ GestionClientes/src/main/java/com/mycompany/gestionclientes/GestionClientes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestionclientes;
+
+/**
+ *
+ * @author danie
+ */
+public class GestionClientes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
